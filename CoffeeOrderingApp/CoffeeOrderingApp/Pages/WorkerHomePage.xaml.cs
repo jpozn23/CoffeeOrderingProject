36f40f44716259e40d6b63b60ea23bde071b20a9
Jordan Poznanski <poznanskijordan@gmail.com>
2023-02-04 17:21:55 -0500

    Added Sign Up Page

--- CoffeeOrderingApp/CoffeeOrderingApp/Pages/WorkerHomePage.xaml.cs
+++ CoffeeOrderingApp/CoffeeOrderingApp/Pages/WorkerHomePage.xaml.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace CoffeeOrderingApp
+namespace CoffeeOrderingApp.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class SignUpPage : ContentPage
+    public partial class WorkerHomePage : ContentPage
     {
-        public SignUpPage()
+        public WorkerHomePage()
         {
             InitializeComponent();
         }
