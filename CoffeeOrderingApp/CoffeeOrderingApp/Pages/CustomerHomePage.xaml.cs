e2835773ed5a7145f4376fd68ba3456547590336
Jordan Poznanski <poznanskijordan@gmail.com>
2023-02-06 01:11:11 -0500

    Added Customer and Worker Home Pages

--- CoffeeOrderingApp/CoffeeOrderingApp/Pages/CustomerHomePage.xaml.cs
+++ CoffeeOrderingApp/CoffeeOrderingApp/Pages/CustomerHomePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoffeeOrderingApp.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,21 @@ namespace CoffeeOrderingApp
         public CustomerHomePage()
         {
             InitializeComponent();
+        }
+
+        async private void AddItemButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CustomerAddItemMenuPage());
+        }
+
+        async private void OrderHistoryButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CustomerOrderHistoryPage());
+        }
+
+        async private void ShoppingCartButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CustomerShoppingCartPage());
         }
     }
 }
